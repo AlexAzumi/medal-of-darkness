1eb8ce5633765bebbd4668a706d70c8d5cf138f9
unknown <alejandro-hdez115@outlook.com>
2018-04-08 20:05:31 -0500

    Edit levels
    Some levels were edited.

--- MedalOfDarkness/Assets/Scripts/Player/CameraFollow.cs
+++ MedalOfDarkness/Assets/Scripts/Player/CameraFollow.cs
@@ -10,6 +10,7 @@ public class CameraFollow : MonoBehaviour
     public float m_Smooting = 5.0f;
     public float m_XOffset = -0.4f;
     public float m_ZOffset = 0.4f;
+    public int m_YOffset = 1;
 
     /* Private stuff */
     private Transform m_Target;
@@ -29,7 +30,7 @@ public class CameraFollow : MonoBehaviour
         m_TargetCamPos = m_Target.position;
 
         if(m_PlayerAsTarget) //Adds offset if the camera is following the player
-            m_TargetCamPos += new Vector3(m_XOffset, 0.0f, m_ZOffset);
+            m_TargetCamPos += new Vector3(m_XOffset, m_YOffset, m_ZOffset);
         
         transform.position = Vector3.Lerp(transform.position, m_TargetCamPos, m_Smooting * Time.deltaTime);	
 	}
