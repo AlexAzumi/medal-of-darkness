41ebfa48fe7f2499315dca31596972b5e36ae4c0
unknown <alejandro-hdez115@outlook.com>
2018-05-11 21:34:39 -0500

    Fix dialog manager script

--- MedalOfDarkness/Assets/Scripts/General/DialogManager.cs
+++ MedalOfDarkness/Assets/Scripts/General/DialogManager.cs
@@ -10,18 +10,22 @@ public class DialogManager : MonoBehaviour
     public GameObject m_DialogBox;
     public Text m_DialogText;
     public PauseMenu m_PauseMenu;
+    public float m_WaitTime;
 
     /* Private stuff */
     private PlayerController m_CharacterControl;
     private bool m_Active, m_ShowAll;
     private string[] m_DialogMessage;
-    private int m_Count;
+    private int m_Count, m_CharPosition;
+    private float m_Timer;
 
 	private void Start() 
     {
         m_CharacterControl = GameObject.Find("Katherine").GetComponent<PlayerController>();
         m_ShowAll = false;
         m_Count = 0;
+        m_CharPosition = 0;
+        m_Timer = m_WaitTime;
 	}
 	
 	private void Update() 
@@ -38,26 +42,51 @@ public class DialogManager : MonoBehaviour
             }
             else
             {
-                m_Count++;
                 ShowNext();
             }
         }
+        ShowMessage();
 	}
+
+    private void ShowMessage()
+    {
+        if (m_Active == true && m_CharPosition < m_DialogMessage[m_Count].Length)
+        {
+            m_Timer -= Time.deltaTime;
+            if (m_Timer < 0)
+            {
+                m_DialogText.text += m_DialogMessage[m_Count][m_CharPosition];
+                m_Timer = m_WaitTime;
+                m_CharPosition++;
+            }
+        }
+    }
 
     private void ShowNext()
     {
-        //Debug.Log(m_Count + " | " + m_DialogMessage.Length);
-        if (m_Count < m_DialogMessage.Length)
+        if (m_CharPosition < m_DialogMessage[m_Count].Length && m_Active == true)
         {
             m_DialogText.text = m_DialogMessage[m_Count];
+            m_CharPosition = m_DialogMessage[m_Count].Length;
+        }
+        else
+        {
+            m_DialogText.text = "";
+            m_CharPosition = 0;
             if (!m_Active)
             {
                 m_CharacterControl.SetCanMove(false);
                 m_DialogBox.SetActive(true);
                 m_Active = true;
             }
+            else
+            {
+                m_Count++;
+            }
             if (m_Count == m_DialogMessage.Length - 1)
+            {
                 m_ShowAll = true;
+            }
         }
     }
 
